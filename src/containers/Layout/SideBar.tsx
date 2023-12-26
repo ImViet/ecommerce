18e23af49ec888895cbe01ef08ceb36be43b58ad
dinh quoc viet <viet.dinh@aba.com.vn>
2023-12-26 16:17:07 +0700

    Add library react-pro-sidebar

--- src/containers/Layout/SideBar.tsx
+++ src/containers/Layout/SideBar.tsx
@@ -1,11 +1,15 @@
-import { Offcanvas } from "react-bootstrap";
+import { Offcanvas, Stack } from "react-bootstrap";
 import { useAppContext } from "../../context/AppContext";
+import { ICategory } from "../../interfaces/ICategory";
+import { Menu, MenuItem } from "react-pro-sidebar";
+import { Link } from "react-router-dom";
 
 interface Props {
   isOpen: boolean;
+  categories: ICategory[];
 }
 
-const SideBar = ({ isOpen }: Props) => {
+const SideBar = ({ isOpen, categories }: Props) => {
   const { closeSideBar } = useAppContext();
   return (
     <Offcanvas
@@ -15,11 +19,29 @@ const SideBar = ({ isOpen }: Props) => {
       onHide={closeSideBar}
     >
       <Offcanvas.Header closeButton>
-        <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+        <Offcanvas.Title></Offcanvas.Title>
       </Offcanvas.Header>
       <Offcanvas.Body>
-        Some text as placeholder. In real life you can have the elements you
-        have chosen. Like, text, images, lists, etc.
+        <Stack gap={3}>
+          <div className="p-1">Shop By Category</div>
+          <div className="p-1">
+            <Menu>
+              {categories &&
+                categories.map((cate) => (
+                  <MenuItem
+                    component={
+                      <Link
+                        to={`categories/${cate.categoryId}/products`}
+                        onClick={closeSideBar}
+                      ></Link>
+                    }
+                  >
+                    {cate.categoryName}
+                  </MenuItem>
+                ))}
+            </Menu>
+          </div>
+        </Stack>
       </Offcanvas.Body>
     </Offcanvas>
   );
